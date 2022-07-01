ed100fd1472fad98be9b96cc2edd1a59a7e3b2c2
sc1357@kit.edu <sc1357@hkn1993.localdomain>
2022-07-01 14:59:17 +0200

    hpc bug fix

--- DLIP/experiments/hpc_jobs/start_train_8.sh
+++ DLIP/experiments/hpc_jobs/start_train_8.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#SBATCH --partition=haicore-gpu4
+#SBATCH --partition=haicore-gpu8
 #SBATCH --time=48:00:00
-#SBATCH --ntasks=152
+#SBATCH --ntasks=256
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:8
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=marcel.schilling@kit.edu
 #SBATCH --error=%j_error.txt
 #SBATCH --output=%j_output.txt
-#SBATCH --job-name=semi_super
+#SBATCH --job-name=ssl
 #SBATCH --constraint=LSDF
 
 export CFG_FILE="/home/hk-project-sppo/sc1357/devel/self-supervised-biomedical-image-segmentation/DLIP/experiments/configurations/base_cfg/semantic/cfg_resunet_sem_seg_base.yaml"
 export RESULT_DIR="/home/hk-project-sppo/sc1357/data/ssl_ref"
-export SWEEPID="marcelschilling/derma_ssl/3nt5qvho"
+export SWEEPID="kit-iai-ibcs-dl/derma_ssl/3nt5qvho"
 
 # remove all modules
 module purge
@@ -32,6 +32,10 @@ cd /home/hk-project-sppo/sc1357/devel/self-supervised-biomedical-image-segmentat
 CUDA_VISIBLE_DEVICES=0 wandb agent $SWEEPID &
 CUDA_VISIBLE_DEVICES=1 wandb agent $SWEEPID &
 CUDA_VISIBLE_DEVICES=2 wandb agent $SWEEPID &
-CUDA_VISIBLE_DEVICES=3 wandb agent $SWEEPID 
+CUDA_VISIBLE_DEVICES=3 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=4 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=5 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=6 wandb agent $SWEEPID &
+CUDA_VISIBLE_DEVICES=7 wandb agent $SWEEPID 
 
 wait < <(jobs -p)
