91cc2852f28529723c6e55251cb27bfdf8840474
Luca <Luca.Rettenberger@kit.edu>
2023-01-23 18:14:47 +0100

    update

--- DLIP/scripts/start_train.sh
+++ DLIP/scripts/start_train.sh
@@ -1,7 +1,4 @@
 export RESULT_DIR="/home/ws/kg2371/projects/self-supervised-biomedical-image-segmentation"
 export CFG_FILE="/home/ws/kg2371/projects/self-supervised-biomedical-image-segmentation/DLIP/experiments/configurations/ssl/breastcancer/train.yaml"
 
-python ssl_breast_train.py --config_files "\
-$CFG_FILE\
-" \
---result_dir $RESULT_DIR
+wandb agent kit-iai-ibcs-dl/ssl_breastcancer/garbuz1d
